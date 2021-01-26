b162f6b10a21c5dc003a2dad3191a9180c8244dd
Srivarshinisachayanthan <76767170+Srivarshinisachayanthan@users.noreply.github.com>
2021-01-26 14:38:20 +0530

    changes

--- JS/onlineStatus.js
+++ JS/onlineStatus.js
@@ -5,4 +5,35 @@ firebase.database().ref('23users/svs23/status/').on('value', (Onlineevent)=> {
     let userStatus = ( "" + (Onlineevent.val().onlineStatus) + " [ " + (Onlineevent.val().lastSeen) + "]" );
     
     $('#OnlineStatus').text(userStatus);
+    
+    let divs = new DivCreator();
+    let values = Date().toString().split(' ');
+    
+    let UniqueIDCreate = new UniqueIdCreator();
+    let ID = UniqueIDCreate.date()  +" "+ UniqueIDCreate.day() +" "+ UniqueIDCreate.month() +" "+ UniqueIDCreate.year() +" "+ UniqueIDCreate.time() ;  
+
+    firebase.database().ref('23users/ssv23/').child('23Messages').child(ID).set({
+        
+        From            : 'Admin',
+        To              : "svs23",
+        Time            : '8.30pm',
+        Date            : "6",
+        Day             : "Fri",
+        Month           : "Sep",
+        Year            : "2019",
+        Message_Type    : "notify",
+        Text_Message    : userStatus,
+        Image_Url       : "NULL",
+        File_Url        : "NULL",
+        File_Name       : "NULL",
+        Video           : "NULL",
+        Audio           : "NULL",
+        Reply_Message   : "NULL",
+        Bolder          : 1,
+        Italic          : 1,
+        Highlight       : 0,
+        Underline       : 0 
+        
+    });
+    
 })
